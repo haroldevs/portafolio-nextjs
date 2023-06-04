ba176f71dd9880a834e4c14cd6a1033c0cccb8fd
haroldevs <haroldthomasg18@gmail.com>
2023-06-04 15:18:29 -0500

    animated text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,57 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import profilePic from "../../public/images/profile/haroldevs.webp";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
+
 export default function Home() {
-  return <main className="">Home</main>;
+  return (
+    <>
+      <Head>
+        <title>Haroldevs</title>
+        <meta name="description" content="Generated portafolio" />
+      </Head>
+      <main className="flex items-center tex-dark w-full min-h-screen">
+        <Layout className="pt-0">
+          <div className="flex items-center justify-betwwen w-full">
+            <div className="w-1/2">
+              <Image
+                src={profilePic}
+                alt="CodeBucks"
+                className="w-full h-auto"
+              />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="text-6xl text-left"
+              />
+              <p className="my-4 text-base font-medium">
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications.
+              </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/haroldcv.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Resume <LinkArrow className="w-6 ml-1" />
+                </Link>
+                <Link
+                  href="mailto:haroldthomasg18@gmail.com"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
 }
